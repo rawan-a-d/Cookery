17635742620aef8b01eca28c8231dc28d1321d86
Rawan Abou Dehn <r.aboudehn@student.fontys.nl>
2020-11-27 01:57:44 +0100

    Fix some sonarqube's issues

--- cookery-service/src/main/java/service/controller/AuthController.java
+++ cookery-service/src/main/java/service/controller/AuthController.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
 public class AuthController {
     private final static Logger LOGGER = Logger.getLogger(AuthController.class.getName());
 
-    private static String SECRET_KEY = "oeRaYY7Wo24sDqKSX3IM9ASGmdGPmkTd9jo1QTy4b7P9Ze5_9hKolVX8xNrQDcNRfVEdTZNOuOyqEGhXEbdJI-ZQ19k_o9MI0y3eZN2lp9jow55FfXMiINEdt1XR85VipRLSOkT6kSpzs2x-jbLDiz9iFVzkd81YKxMgPA7VfZeQUm4n-mOmnWMaVX30zGFU4L3oPBctYKkl4dYfqYWqRNfrgPJVi5DGFjywgxx0ASEiJHtV72paI3fDR2XwlSkyhhmY-ICjCRmsJN4fX1pdoL8a18-aQrvyu4j0Os6dVPYIoPvvY0SAZtWYKHfM15g7A3HD4cVREf9cUsprCRK93w";
+    private static String secretKey = "oeRaYY7Wo24sDqKSX3IM9ASGmdGPmkTd9jo1QTy4b7P9Ze5_9hKolVX8xNrQDcNRfVEdTZNOuOyqEGhXEbdJI-ZQ19k_o9MI0y3eZN2lp9jow55FfXMiINEdt1XR85VipRLSOkT6kSpzs2x-jbLDiz9iFVzkd81YKxMgPA7VfZeQUm4n-mOmnWMaVX30zGFU4L3oPBctYKkl4dYfqYWqRNfrgPJVi5DGFjywgxx0ASEiJHtV72paI3fDR2XwlSkyhhmY-ICjCRmsJN4fX1pdoL8a18-aQrvyu4j0Os6dVPYIoPvvY0SAZtWYKHfM15g7A3HD4cVREf9cUsprCRK93w";
 
 
     /*---------------------------------------------------------------- Authenticate ----------------------------------------------------------------------*/
@@ -60,29 +60,18 @@ public class AuthController {
         Date now = new Date(nowMillis);
 
         //We will sign our JWT with our ApiKey secret
-        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secretKey);
         Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
 
-        boolean isAdmin = user.getRole().equals(Role.admin) ? true : false;
-        //Let's set the JWT Claims
+        boolean isAdmin = user.getRole().equals(Role.admin);
+
         JwtBuilder builder = Jwts.builder()
-//                .setId(id)
                 .setIssuedAt(now)
                 .setSubject(Integer.toString(user.getId()))
-//                .setIssuer(issuer)
                 .signWith(signatureAlgorithm, signingKey)
-//                .setExpiration("7 days")
                 .claim("name", user.getName())
                 .claim("admin", isAdmin);
 
-
-
-        //if it has been specified, let's add the expiration
-//        if (ttlMillis > 0) {
-//            long expMillis = nowMillis + ttlMillis;
-//            Date exp = new Date(expMillis);
-//            builder.setExpiration(exp);
-//        }
 
         //Builds the JWT and serializes it to a compact, URL-safe string
         return builder.compact();
@@ -93,23 +82,19 @@ public class AuthController {
 
         //This line will throw an exception if it is not a signed JWS (as expected)
         Claims claims = Jwts.parser()
-                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .setSigningKey(DatatypeConverter.parseBase64Binary(secretKey))
                 .parseClaimsJws(jwt).getBody();
 
-        System.out.println("Claims");
-        System.out.println(claims);
         return claims;
     }
 
 
     public int getIdInToken(String auth) {
-        String AUTHENTICATION_SCHEME = "Bearer"; // the scheme (value starts with Basic)
-        String encodedCredentials = auth.replaceFirst(AUTHENTICATION_SCHEME + " ", ""); // remove scheme (Basic) and space
+        String authenticationScheme = "Bearer"; // the scheme (value starts with Basic)
+        String encodedCredentials = auth.replaceFirst(authenticationScheme + " ", ""); // remove scheme (Basic) and space
 
         Claims decodedToken = AuthController.decodeJWT(encodedCredentials);
 
-        int id = Integer.parseInt(decodedToken.get("sub").toString());
-
-        return id;
+        return Integer.parseInt(decodedToken.get("sub").toString());
     }
 }
